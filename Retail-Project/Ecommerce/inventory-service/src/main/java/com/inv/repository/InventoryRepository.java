a8ca1d7d85b9cf958542073dbff1dd0cd26ae69b
raghavendraavidi2003 <avidi.raghavendra@encora.com>
2025-10-08 10:42:19 +0530

    Delete Retail-Project/Ecommerce directory

--- Retail-Project/Ecommerce/inventory-service/src/main/java/com/inv/repository/InventoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.inv.repository;
-
-
-import com.inv.model.InventoryItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InventoryRepository extends JpaRepository<InventoryItem, String> {
-}
